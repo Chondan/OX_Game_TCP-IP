580c7af6f5c9f52cc9d947bc8fe3a22a08a6645b
Chondan Susuwan <50775454+Chondan@users.noreply.github.com>
2020-08-24 17:36:17 +0700

    Update Client.cs

--- Client/Client.cs
+++ Client/Client.cs
@@ -24,7 +24,6 @@ namespace Client
 
         class client_class
         {
-
             public string client_name;
             public string client_message;
 
@@ -42,24 +41,15 @@ namespace Client
                 {
 
                     TcpClient myclient = new TcpClient();
-                    myclient.Connect(IPAddress.Parse(ClientHostTB.Text), Convert.ToInt32(ClientPortTb.Text));
-                    
-                    netStream = myclient.GetStream();
-                    
-
+                    myclient.Connect(IPAddress.Parse(ClientHostTB.Text), Convert.ToInt32(ClientPortTb.Text));                 
+                    netStream = myclient.GetStream();                   
                     Byte[] sendBytes = Encoding.UTF8.GetBytes(ClientNameTB.Text);
-                    netStream.Write(sendBytes, 0, sendBytes.Length);
-
-                    
-
+                    netStream.Write(sendBytes, 0, sendBytes.Length);                
                     label3.Text = "Connected";
                     ConnectBT.Enabled = false;
                     //timer1.Start();
                     timer2.Start();
                     chk = 1;
-                    
-
-
                 }
                 catch
                 {
@@ -69,15 +59,11 @@ namespace Client
             else
             {
                 MessageBox.Show("Please insert your name");
-            }
-            
+            }         
         }
-
         private void ClientSendBT_Click(object sender, EventArgs e)
-        {
-            
+        {           
             if (netStream == null) return;
-
             if (netStream.CanWrite)
             {
                 client_class tmp = new client_class();
@@ -85,34 +71,22 @@ namespace Client
                 tmp.client_name = ClientNameTB.Text;
                 tmp.client_message = ClientChatTB.Text;
                 client_list.Add(tmp);
-
                 foreach (var clnt in client_list)
-                {
-                    
+                {                
                     Byte[] sendBytes = Encoding.UTF8.GetBytes(tmp.client_name + "\r\n" + ClientChatTB.Text + "\r\n");
                     netStream.Write(sendBytes, 0, sendBytes.Length);
-
-
-                }
-               
-                
-               
+                }             
             }
-            ClientChatTB.Text = "";
-          
+            ClientChatTB.Text = "";        
         }
-
         private void ClientScreenTB_KeyUp(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
             {
                 ClientSendBT_Click(this, null);
                 ClientChatTB.Text = "";
-
             }
-
         }
-
         private void DisconnectBT_Click(object sender, EventArgs e)
         {
             timer2.Stop();
@@ -120,11 +94,7 @@ namespace Client
             netStream.Dispose();
             ClientNameTB.Enabled = true;
             ConnectBT.Enabled = true;
-
-            
-
         }
-
         private void timer1_Tick(object sender, EventArgs e)
         {
             //ClientScreenTB.Text = "";
@@ -134,45 +104,29 @@ namespace Client
                 netStream.ReadAsync(bytes, 0, 256);
                 string returndata = Encoding.UTF8.GetString(bytes);
                 ClientScreenTB.Text = returndata;
-
             }
             timer1.Stop();
-
         }
-
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
-        {
-           
-           
+        {   
         }
-
         private void timer2_Tick(object sender, EventArgs e)
-        {
-            
-            
+        {  
             if (netStream.DataAvailable)
             {
-
                 byte[] bytes = new byte[256];
                 netStream.ReadAsync(bytes, 0, 256);
-                string returndata = Encoding.UTF8.GetString(bytes);
-
-                
+                string returndata = Encoding.UTF8.GetString(bytes);               
                 PlayerLabel.Text = returndata;
                 timer2.Stop();
-
-
-
                 //timer1.Start();
             }
-
         }
         int board_size = 3;
         int margin_left = 10;
         int margin_top = 10;
         int tile_width = 20;
         //int turn = 1;
-
         int[,] board;
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
@@ -188,13 +142,9 @@ namespace Client
                     , margin_left + tile_width * board_size, margin_top + i * tile_width
 
                     );
-
             }
             if (start == 1)
             {
-
-
-
                 for (int row = 0; row < board_size; row++)
                     for (int col = 0; col < board_size; col++)
                     {
@@ -210,34 +160,18 @@ namespace Client
                         else
                         {
                             b = Brushes.Blue;
-                        }
-                        
-
-
+                        }                     
                         if (board[row, col] != 0)
                         {
                             g.FillEllipse(b, margin_left + col * tile_width, margin_top + row * tile_width
                                 , tile_width, tile_width);
                         }
 
-
-
-
                     }
-
-
-
-            }
-
-            
-        }
-        
-        
+            }          
+        }            
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            
-           
-
         }
         int start;
         int pos;
@@ -247,15 +181,11 @@ namespace Client
         {
             if (chk == 1)
             {
-
-
                 PlayGameBT.BackColor = Color.LimeGreen;
                 checkfull = 0;
                 start = 1;
                 press = 1;
                 board = new int[board_size, board_size];
-
-
                 if (PlayerLabel.Text == "A")
                 {
                     pos = 1;
@@ -272,7 +202,6 @@ namespace Client
                     {
                         board[row, col] = 0;
                     }
-
                 //board[1, 2] = 1;
                 //board[0, 1] = -1;
                 Refresh();
@@ -281,18 +210,12 @@ namespace Client
             {
                 MessageBox.Show("Please connect to the server");
             }
-        }
-            
-
-
-        int check;
-        
+        }           
+        int check;     
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
-        {
-            
+        {      
             int row = (e.Y - margin_top) / tile_width;
             int col = (e.X - margin_left) / tile_width;
-
             if (press == 1)
             {
                 judge();
@@ -304,14 +227,9 @@ namespace Client
                         {
                             full += 1;
                         }
-
                     }
-
                 if (checkfull != 1 && full != 9)
                 {
-
-
-
                     if (turn == 1 && PlayerLabel.Text == "B")
                     {
                         if (board[row, col] != 1 && board[row, col] != -1)
@@ -320,39 +238,20 @@ namespace Client
                             Byte[] sendBytes = Encoding.UTF8.GetBytes("B" + "\r\n" + row + "\r\n" + col);
                             netStream.Write(sendBytes, 0, sendBytes.Length);
                         }
-
-
-
                     }
                     else if (turn == 0 && PlayerLabel.Text == "A")
                     {
-
                         if (board[row, col] != 1 && board[row, col] != -1)
                         {
                             board[row, col] = pos;
                             Byte[] sendBytes = Encoding.UTF8.GetBytes("A" + "\r\n" + row + "\r\n" + col);
                             netStream.Write(sendBytes, 0, sendBytes.Length);
                         }
-
-
-
-
                     }
                     else
                     {
-
                         MessageBox.Show("It's not your turn");
-
-
                     }
-
-
-
-
-
-
-
-
                     //if (PlayerLabel.Text == "A")
                     //{
                     //    Byte[] sendBytes = Encoding.UTF8.GetBytes("A" + "\r\n" + row + "\r\n" + col);
@@ -366,9 +265,6 @@ namespace Client
                     //    netStream.Write(sendBytes, 0, sendBytes.Length);
 
                     //}
-
-
-
                     Refresh();
                     //MessageBox.Show(row.ToString());
                     //MessageBox.Show(col.ToString());
@@ -405,14 +301,12 @@ namespace Client
                 MessageBox.Show("Please press 'PLAY GAME' ");
             }
         }
-
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
             label5.Text = e.X.ToString();
             label6.Text = e.Y.ToString();
 
         }
-
         private void Client_Load(object sender, EventArgs e)
         {
             //board = new int[board_size, board_size];
@@ -420,8 +314,6 @@ namespace Client
             //board[1, 2] = 1;
             //board[0, 1] = -1;
         }
-        
-       
         protected string[] lines; 
         private void timer3_Tick(object sender, EventArgs e)
         {
@@ -439,16 +331,11 @@ namespace Client
                     ClientScreenTB.Text = "";
 
                     //timer3.Start();
-
                 }
             }
             catch
             {
-
             }
-
-
-
         }
 
         private void PlayerLabel_Click(object sender, EventArgs e)
@@ -463,16 +350,12 @@ namespace Client
             board[2, 0] = int.Parse(lines[6]);
             board[2, 1] = int.Parse(lines[7]);
             board[2, 2] = int.Parse(lines[8]);
-
-
+            
             check = 0;
             for (int i = 0; i < 9; i++)
             {
                 check += int.Parse(lines[i]);
-
-
             }
-
             Refresh();
             MessageBox.Show(check.ToString());
         }
@@ -493,7 +376,6 @@ namespace Client
             {
                 textBox1.BackColor = Color.Blue;
             }
-
         }
 
         public int checkfull = 0;
@@ -503,8 +385,6 @@ namespace Client
 
             try
             {
-
-
                 for (int row = 0; row < 3; row++)
                 {
                     if (board[row, 0] == board[row, 1])
@@ -515,11 +395,9 @@ namespace Client
                             {
                                 checkfull = 1;
                                 if (board[row, 0] == 1)
-                                {
-                                    
+                                {                                 
                                     winner = 1;
                                     //MessageBox.Show("A win");
-
                                 }
                                 else
                                 {
@@ -540,11 +418,9 @@ namespace Client
                             {
                                 checkfull = 1;
                                 if (board[0, col] == 1)
-                                {
-                                    
+                                {       
                                     winner = 1;
                                     //MessageBox.Show("A win");
-
                                 }
                                 else
                                 {
@@ -567,7 +443,6 @@ namespace Client
                                 
                                 winner = 1;
                                 //MessageBox.Show("A win");
-
                             }
                             else
                             {
@@ -586,10 +461,8 @@ namespace Client
                             checkfull = 1;
                             if (board[0, 2] == 1)
                             {
-                                
                                 winner = 1;
                                 //MessageBox.Show("A win");
-
                             }
                             else
                             {
@@ -599,13 +472,10 @@ namespace Client
                         }
                     }
                 }
-
             }
             catch
             {
-
             }
-
             int full = 0;
             for (int row = 0; row < board_size; row++)
                 for (int col = 0; col < board_size; col++)
@@ -628,7 +498,6 @@ namespace Client
         {
             try
             {
-
                 board[0, 0] = int.Parse(lines[0]);
                 board[0, 1] = int.Parse(lines[1]);
                 board[0, 2] = int.Parse(lines[2]);
@@ -638,39 +507,25 @@ namespace Client
                 board[2, 0] = int.Parse(lines[6]);
                 board[2, 1] = int.Parse(lines[7]);
                 board[2, 2] = int.Parse(lines[8]);
-
-
                 check = 0;
                 for (int i = 0; i < 9; i++)
                 {
                     check += int.Parse(lines[i]);
-
-
                 }
-
                 Refresh();
-
-               
-
             }
             catch
             {
-
             }
             if (check == 1)
             {
-
                 turn = 1;
                 label7.Text = "B turn";
-
-
             }
             else
             {
                 turn = 0;
                 label7.Text = "A turn";
-
-
             }
 
         }
